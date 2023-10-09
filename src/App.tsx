ffff24711ef22a89165e5502516abed164b16610
avi-edgeware <avi.michael@edgewaretech.com.au>
2023-10-09 16:55:14 +1100

    create-react-app tailwind daisyui

---
+++ src/App.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <article className="prose lg:prose-xl">
+        <h1>Garlic bread with cheese: What the science tells us</h1>
+        <p>
+          For years parents have espoused the health benefits of eating garlic
+          bread with cheese to their children, with the food earning such an
+          iconic status in our culture that kids will often dress up as warm,
+          cheesy loaf for Halloween.
+        </p>
+        <p>
+          But a recent study shows that the celebrated appetizer may be linked
+          to a series of rabies cases springing up around the country.
+        </p>
+      </article>
+      {/* Open the modal using document.getElementById('ID').showModal() method */}
+
+      <button
+        className="btn"
+        onClick={
+          //@ts-ignore
+          () => document.getElementById("my_modal_1").showModal()
+        }
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_1" className="modal">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
+}
+
+export default App;
